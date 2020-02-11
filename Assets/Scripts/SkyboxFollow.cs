7766fdc5b7b0cf2181bf8618cbfb26fc38f43c0f
MaddieFish <MaddieFish@users.noreply.github.com>
2020-02-11 03:54:46 -0500

    Added portals between worlds

---
+++ Assets/Scripts/SkyboxFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Assets/ThreeInOne/PortalTextureSetup.cs
@@ -0,0 +1,55 @@
+﻿//Reference: Brackeys Smooth Portals in Unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera Underwater360Camera;
+    public Material CameraMat_Underwater;
+
+    public Camera BeachGameWorldCameraUnderwater;
+    public Material CameraMat_BeachGameWorldUnderwater;
+
+    public Camera BeachRec360Camera;
+    public Material CameraMat_BeachRec;
+
+    public Camera BeachGameWorldCameraRec;
+    public Material CameraMat_BeachGameWorldRec;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Underwater360Camera.targetTexture != null)
+        {
+            Underwater360Camera.targetTexture.Release();
+        }
+        Underwater360Camera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMat_Underwater.mainTexture = Underwater360Camera.targetTexture;
+
+        if (BeachGameWorldCameraUnderwater.targetTexture != null)
+        {
+            BeachGameWorldCameraUnderwater.targetTexture.Release();
+        }
+        BeachGameWorldCameraUnderwater.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMat_BeachGameWorldUnderwater.mainTexture = BeachGameWorldCameraUnderwater.targetTexture;
+
+
+        if (BeachRec360Camera.targetTexture != null)
+        {
+            BeachRec360Camera.targetTexture.Release();
+        }
+        BeachRec360Camera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMat_BeachRec.mainTexture = BeachRec360Camera.targetTexture;
+
+        if (BeachGameWorldCameraRec.targetTexture != null)
+        {
+            BeachGameWorldCameraRec.targetTexture.Release();
+        }
+        BeachGameWorldCameraRec.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMat_BeachGameWorldRec.mainTexture = BeachGameWorldCameraRec.targetTexture;
+    }
+
+}
