2a81dae80f1690f8e7c2880fc32e57ac7cecc320
MaddieFish <MaddieFish@users.noreply.github.com>
2020-02-25 13:17:00 -0500

    Setup more artifacts

---
+++ Assets/ThreeInOne/Artifact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artifact : MonoBehaviour
+{
+    Transform otherArtifactTrans;
+
+    Collider artifactCol;
+    Vector3 otherArtifactPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        artifactCol = gameObject.GetComponentInChildren<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 otherArtifactPos = otherArtifactTrans.position;
+
+        if (artifactCol.bounds.Contains(otherArtifactPos))
+        {
+
+        };
+
+    }
+}
