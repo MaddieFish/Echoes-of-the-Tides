4f5bcb5d0829ab4dbb0590df7ad5b616c3c236e5
MaddieFish <MaddieFish@users.noreply.github.com>
2020-03-30 02:27:59 -0400

    Rough Animation for Strangled Turtle and Fish

---
+++ Assets/Scripts/SpawnStrangledTurtle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStrangledTurtle : MonoBehaviour
+{
+    public GameObject StrangledTurtlePrefab;
+    GameObject strangled;
+    public List<string> underWaterCollections = new List<string>();
+    public bool strangledTurtlePlaced;
+    public GameObject ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ground = GameObject.FindWithTag("Ground");
+
+        if (ground != null)
+        {
+            //underWaterCollections = ground.GetComponent<CollectionsIn360Scene>().underWaterCollections;
+            //underWaterCollections = ground.GetComponent<ArtifactProximity>().underWaterCollections;
+            underWaterCollections = ground.GetComponent<Collections>().underWaterCollections;
+
+
+            strangled = Instantiate(StrangledTurtlePrefab);
+            strangled.SetActive(false);
+            strangled.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (underWaterCollections.Contains("Strangled Turtle") == true && strangledTurtlePlaced == false)
+        {
+            strangledTurtlePlaced = true;
+            strangled.SetActive(true);
+
+            //strangled = Instantiate(StrangledFishPrefab);
+            //strangled.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (strangledTurtlePlaced == true && underWaterCollections.Contains("Strangled Turtle") == false)
+        {
+            //Destroy(strangled);
+            strangledTurtlePlaced = false;
+            strangled.SetActive(false);
+
+        }
+    }
+}
