947c5b42a4da5c447064ee2f756be6845e306b4c
MaddieFish <MaddieFish@users.noreply.github.com>
2020-04-01 00:05:27 -0400

    Added OnGrab script to test for disabling kinematics on interactor grab

---
+++ Assets/ThreeInOne/OnGrab.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class OnGrab : MonoBehaviour
+{
+    private XRGrabInteractable grabInteractable;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        rigidbody = GetComponent< Rigidbody > ();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+
+    }
+
+    public void RemoveFromInvent()
+    {
+        rigidbody.isKinematic = false;
+        grabInteractable.gameObject.transform.parent = null;
+        grabInteractable.gameObject.transform.SetParent(null);
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
