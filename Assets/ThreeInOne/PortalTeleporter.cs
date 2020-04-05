e1357f72163a944227459f2c45904567026af29b
MaddieFish <MaddieFish@users.noreply.github.com>
2020-04-05 00:33:46 -0400

    Revert "Revert "Added Hector's PortalTeleporter Script changes to change the bucket to kinematic on enter""
    This reverts commit 7d0dcef773e64d1131fe705773f503aa4ee73180.

--- Assets/ThreeInOne/PortalTeleporter.cs
+++ Assets/ThreeInOne/PortalTeleporter.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PortalTeleporter : MonoBehaviour
 {
@@ -11,18 +12,44 @@ public class PortalTeleporter : MonoBehaviour
 
     public bool playerIsOverlapping = false;
     public float dotProduct;
+    private Rigidbody PailRB;
+
+    void Start()
+    {
+        GameObject Pail = GameObject.Find("Pail Body Square");
+        if (Pail != null)
+        {
+            PailRB = Pail.GetComponent<Rigidbody>();
+        }
+    }
+
+    /*
+    void LateUpdate()
+    {
+        
+    }
+    */
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        if (PailRB.isKinematic == true)
+        {
+            PailRB.isKinematic = false;
+        }
+
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
             dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
             //If this is true the player has moved across the portal
-            if (dotProduct < 0f){
+            if (dotProduct < 0f)
+            {
+
                 //teleport
+                PailRB.isKinematic = true;
+
                 float rotationDiff = Quaternion.Angle(transform.rotation, reciever.rotation);
                 rotationDiff += 180;
                 player.Rotate(Vector3.up, rotationDiff);
